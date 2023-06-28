208672902c16e3f5d47d7be5939068380c27b07b
Valentin Voica <valentin_voiclla@live.com>
2023-06-28 17:07:21 +0300

    Add project files.

---
+++ lab2ee1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab2ee1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti latimea");
+            var latime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti lungimea");
+            var lungimea = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti inaltimea");
+            var inaltimea = int.Parse(Console.ReadLine());
+
+            var volum = lungimea * latime * inaltimea;
+            Console.WriteLine("Volumul este " + volum);
+
+        }
+    }
+}
+
